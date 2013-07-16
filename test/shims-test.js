04ff2b5150c0209ca1ec0eff9817f16cd1b8d89e
Lauren Sperber <lauren.sperber@gmail.com>
2013-07-16 14:43:03 -0400

    another indentation fix

--- test/shims-test.js
+++ test/shims-test.js
@@ -1,21 +1,21 @@
 var assert = require('assert');
 
 describe('Array', function () {
-    beforeEach(function () {
-        Array.prototype.map = undefined;
-        Array.prototype.filter = undefined;
-        var shims = require('../js/shims');
-    });
+  beforeEach(function () {
+    Array.prototype.map = undefined;
+    Array.prototype.filter = undefined;
+    var shims = require('../js/shims');
+  });
 
-    describe('#map()', function () {
-        it('should return an array that is the result of executing the provided function on the current array', function () {
-            assert.deepEqual([2,4,6], [1,2,3].map(function (num) { return num * 2; }));
-        });
+  describe('#map()', function () {
+    it('should return an array that is the result of executing the provided function on the current array', function () {
+      assert.deepEqual([2,4,6], [1,2,3].map(function (num) { return num * 2; }));
     });
+  });
 
-    describe('#filter()', function () {
-        it('it should only return items that are <5 in passed array', function () {
-            assert.deepEqual([1,2,3,4], [1,2,3,4,5,6,7,8].filter(function (x) {return x < 5;}));
-        });
+  describe('#filter()', function () {
+    it('it should only return items that are <5 in passed array', function () {
+      assert.deepEqual([1,2,3,4], [1,2,3,4,5,6,7,8].filter(function (x) {return x < 5;}));
     });
+  });
 });

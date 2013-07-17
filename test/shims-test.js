693373cc775714d69e0b801423441deeac427906
Lauren Sperber <lauren.sperber@gmail.com>
2013-07-17 14:23:41 -0400

    better description for filter test

--- test/shims-test.js
+++ test/shims-test.js
@@ -14,7 +14,7 @@ describe('shims', function () {
   });
 
   describe('#customFilter()', function () {
-    it('it should only return items that are <5 in passed array', function () {
+    it('it should only return items for which the provided function evaluates to true', function () {
       assert.deepEqual([1,2,3,4,4,3,2,1], [1,2,3,4,5,6,7,8,4,3,2,1].filter(function (x) {return x < 5;}));
       assert.deepEqual([], [8,6,34,26,7,84].filter(function (x) {return x < 5;}));
     });
